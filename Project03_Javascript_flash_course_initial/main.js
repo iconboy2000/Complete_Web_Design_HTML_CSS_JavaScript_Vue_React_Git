875b28948c887a99af769afb9c89ebca7c28e21e
iconboy2000 <iconboy2000@gmail.com>
2026-01-20 22:04:48 +0900

    Modify: celsius & fahr Function 추가

--- Project03_Javascript_flash_course_initial/main.js
+++ Project03_Javascript_flash_course_initial/main.js
@@ -9,11 +9,26 @@ document.querySelector("#close-nav-menu").addEventListener("click", function() {
 });
 
 // Greeting Section - Show current date and time
+function celsiusToFahr(temperature) {
+    let fahr = (temperature * 9/5) + 32;
+    return fahr;
+}
+
 const greetingText = "Good morning!";
 const weatherCondition = "sunny";
 const userLocation = "New York";
-let temperature = 22.8673; 
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${temperature.toFixed(1)}°C outside.`;
+let temperature = 30; 
+
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${temperature}°C outside.`;
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
+document.querySelector("p#weather").innerHTML = celsiusText;
+
+document.querySelector(".weather-group").addEventListener("click", function(event) {
+    if (event.target.id == "celsius") {
+        document.querySelector("p#weather").innerHTML = celsiusText;
+    } else if (event.target.id == "fahr") {
+        document.querySelector("p#weather").innerHTML = fahrText;
+    } 
+});
